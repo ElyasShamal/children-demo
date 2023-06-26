b52ca200db9444b9a6039183ea5060d4b08e3b3f
Elyas <Elyas.shamal2@gmail.com>
2023-06-26 18:01:18 -0400

    children

--- src/Card.js
+++ src/Card.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from "react";
 
 const cardStyle = {
-    width: '50%',
-    margin: '10px auto 30px auto',
-    boxShadow: '0 5px 10px 2px rgba(0,0,0,0.25)',
-    padding: '20px'
-}
+  width: "50%",
+  margin: "10px auto 30px auto",
+  boxShadow: "0 5px 10px 2px rgba(0,0,0,0.25)",
+  padding: "20px",
+};
 
-const Card = ({children, ...props}) => {
-    console.log(props)
+const Card = ({ children, title, description }) => {
   return (
     <div style={cardStyle}>
-        {props.title}
-        {props.description && <p>{props.description}</p>}
-        {children}
+      {title}
+      {description && <p>{description}</p>}
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
